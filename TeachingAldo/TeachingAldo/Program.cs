c565105cb111fef71e0c1edbbc1d8f90da6021a2
thefont <eric.m.font@gmail.com>
2013-11-22 22:50:45 -0600

    Pushing our car project so aldo can play with it at home.

---
+++ TeachingAldo/TeachingAldo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TeachingAldo
+{
+    using System.Security.Cryptography.X509Certificates;
+
+    public class Program
+    {
+        static void Main()
+        {
+            int milesToChurch = 13;
+
+            Car bmw = new Car();
+
+            bmw.Drive(milesToChurch);
+            Console.WriteLine(String.Format("Odometer Before shady mechanic: {0}", bmw.odometer));
+
+            ShadyMechanic bob = new ShadyMechanic();
+            bob.rollBackOdometer(bmw);
+
+            Console.WriteLine(String.Format("Odometer After shady mechanic: {0}", bmw.odometer));
+            
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class Car
+    {
+        public string make { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+        public bool hasABS { get; set; }
+        public bool hasPowerWindows { get; set; }
+        public int odometer { get; set; }
+
+        public Car()
+        {
+            make = "BMW";
+            model = "M3";
+            year = 2014;
+            hasABS = true;
+            hasPowerWindows = false;
+            odometer = 0;
+        }
+
+        public void Drive(int tripDistance)
+        {
+            odometer += tripDistance;
+        }
+    }
+
+    public class ShadyMechanic
+    {
+        public Car rollBackOdometer(Car car)
+        {
+            car.odometer = 0;
+
+            return car;
+        }
+    }
+}
